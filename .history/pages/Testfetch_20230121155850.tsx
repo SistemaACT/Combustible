e1891fc21ff5fb55abaeb9308bcd8460ea0e7c3b
Cotizacionact <cotizacionact@gmail.com>
2023-01-21 16:00:22 -0600

    image fixed 15

--- .history/pages/Testfetch_20230121155850.tsx
+++ .history/pages/Testfetch_20230121155850.tsx
@@ -21,8 +21,6 @@ export default function Testfetch() {
     <div>
       <Image src="https://drive.google.com/uc?export=view&id=1-3Dh6B5Y8XE-OkzR8hT6BqWXP_V129cT" width={400} height={50} alt="Testing"></Image>
       <Image src="https://drive.google.com/uc?export=view&id=1vh7AuMpEAQJylq_Gbq2_z5fEfgpcOPnE" width={400} height={50} alt="Testing"></Image>
-      <Image alt="Image" width={200} height={20} src="https://drive.google.com/uc?export=view&id=1cUorMu4O2N-wRLnQfjYtNxo6onZYNft5"></Image>
-
     </div>
   )
 }
