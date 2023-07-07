66fdf346c050bead9f944fd4db3bd08af2c016e3
Cotizacionact <cotizacionact@gmail.com>
2023-07-07 18:10:12 -0500

    fixing code

--- DMS V2/Mutations.ts
+++ DMS V2/Mutations.ts
@@ -11,7 +11,7 @@ export async function RunScript( fn:string,data:Object){
     let res
 
     try{
-       res =  await fetch("https://script.google.com/macros/s/AKfycbxiphQpAuYPXrcLv-PJIZKUYrUyjWrHJ_JVxMPgTOgynz5LFcF6wgF0S5aMbVZQ96lydQ/exec",{
+       res =  await fetch("https://script.google.com/macros/s/AKfycbz1huHnbJDc0TPvl6gZ0zg_0H3qPllnepIJeHrHh8c14ZFJrRXbQUyuaYHpX5WXNxyINw/exec",{
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             body: JSON.stringify({functionName:fn ,data:data, APIKey: "aVyf96paVJWiXs3Nyg6V"}) // body data type must match "Content-Type" header
         })
